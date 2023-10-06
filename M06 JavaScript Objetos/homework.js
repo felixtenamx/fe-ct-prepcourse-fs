6253465805b22fc8bbe965d90e4a83cf5209afac
felixtenamx <felixtheft@gmail.com>
2023-10-05 23:08:50 -0600

    actualización, terminación de ejercicios carpeta M06

--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -45,20 +45,18 @@ function invocarMetodo(objeto, metodo) {
    // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
    // [NOTA]: no necesitar retornar nada.
    // Tu código:
+   objeto[metodo]()
    
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
    // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
    // Debes multiplicar este número por 5 y retornar el resultado.
-   // Tu código:
-var objetoMisterioso = {
-   NumeroMisterioso: 5
-}
+   // Tu código: 
 
-   var resultado = objetoMisterioso.NumeroMisterioso * 5;
+   var resultado = objetoMisterioso.numeroMisterioso * 5;
 
-   console.log(resultado);
+   return resultado;
 
 
    
@@ -81,18 +79,36 @@ function tieneEmail(objetoUsuario) {
    // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   //var tiene = objetoUsuario.hasOwnProperty('email');
+   if (objetoUsuario['email']){
+      return true
+   }else {
+      return false
+   }
 }
 
 function tienePropiedad(objeto, propiedad) {
    // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   //var tiene = objeto.hasOwnProperty('propiedad');
+   if (objeto[propiedad]){
+      return true
+   }else {
+      return false
+   }
 }
 
 function verificarPassword(objetoUsuario, password) {
    // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   //var tiene = ;
+   if(objetoUsuario.password === password){
+      return true
+   }else {
+      return false
+   }
 }
 
 function actualizarPassword(objetoUsuario, nuevaPassword) {
@@ -100,6 +116,9 @@ function actualizarPassword(objetoUsuario, nuevaPassword) {
    // La nueva contraseña la recibes por parámetro.
    // Retornar el objeto.
    // Tu código:
+   objetoUsuario.password = nuevaPassword
+
+   return objetoUsuario;
 }
 
 function agregarAmigo(objetoUsuario, nuevoAmigo) {
@@ -107,6 +126,8 @@ function agregarAmigo(objetoUsuario, nuevoAmigo) {
    // Debes agregar el "nuevoAmigo" al final de este arreglo.
    // Retornar el objeto.
    // Tu código:
+   objetoUsuario.amigos.push(nuevoAmigo);
+   return objetoUsuario;
 }
 
 function pasarUsuarioAPremium(objetoMuchosUsuarios) {
@@ -115,6 +136,19 @@ function pasarUsuarioAPremium(objetoMuchosUsuarios) {
    // Define esta propiedad de todos los usuarios como true.
    // Retornar el arreglo.
    // Tu código:
+   //var objetoMuchisUsuarios = {
+      //usuarios: {
+       //  usuario1: esPremium,
+         //usuario2: esPremium
+      //}
+   //}
+   for (let i = 0; i < objetoMuchosUsuarios.length; i++) {
+      var usuarios = objetoMuchosUsuarios[i];
+      usuarios.esPremium = true
+      
+   }
+
+   return objetoMuchosUsuarios;
 }
 
 function sumarLikesDeUsuario(objetoUsuario) {
@@ -123,6 +157,12 @@ function sumarLikesDeUsuario(objetoUsuario) {
    // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
    // Debes sumar los likes de todos los post y retornar el resultado.
    // Tu código:
+   var total = 0
+   objetoUsuario.posts.forEach(function(post) {
+      total = total + post.likes
+   })
+
+   return total
 }
 
 function agregarMetodoCalculoDescuento(objetoProducto) {
@@ -136,6 +176,12 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
    // PorcentajeDeDescuento ---> 0.2
    // Precio final ---> 8
    // Tu código:
+   objetoProducto.calcularPrecioDescuento = function() { 
+      return this.precio - this.precio * this.porcentajeDeDescuento;
+
+   };
+
+   return objetoProducto; 
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
